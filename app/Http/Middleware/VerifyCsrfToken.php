2421469294ef5778ed45195392b4dd6490ce7c1b
onishellti <93847678+onishellti@users.noreply.github.com>
2021-11-13 09:53:14 -0400

    Update VerifyCsrfToken.php

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        'https://sistema-visitantes.herokuapp.com/login'
+        'https://sistema-visitantes.herokuapp.com/dashboard'
     ];
 }
